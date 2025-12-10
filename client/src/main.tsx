6a60d0fc22e723729087706d232fe90a7954d5a2
Waelawady <waelawady@gmail.com>
2025-12-10 16:16:50 +0300

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,5 @@
 import { trpc } from "@/lib/trpc";
-import { UNAUTHED_ERR_MSG } from '@shared/const';
+import { APP_TITLE, APP_LOGO, ANALYTICS_ENDPOINT, ANALYTICS_WEBSITE_ID } from "@shared/const";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { httpBatchLink, TRPCClientError } from "@trpc/client";
 import { createRoot } from "react-dom/client";
